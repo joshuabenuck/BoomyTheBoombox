87917a7af266d82b3cd50eee9714f40670cd2c7d
Joshua Benuck <joshua_benuck@yahoo.com>
2017-08-06 22:57:40 -0700

    Added upload / download / remove support.

--- BoomySketch/BoomySketch.ino
+++ BoomySketch/BoomySketch.ino
@@ -6,6 +6,8 @@ SdFat SD;
 #include <IRrecv.h>
 #include <IRutils.h>
 #include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
 #include <PubSubClient.h>
 
 int RECV_PIN = 4; //an IR detector/demodulatord is connected to GPIO pin 2
@@ -31,13 +33,22 @@ char broker_ip[20];
 char ssid[32];
 char password[32];
 
-WiFiServer server(80);
 WiFiClient client;
 PubSubClient mqtt(client);
+
+// Used by MP3 fns
 
 File currentTrack;
 boolean playing = false;
 uint8_t mp3buffer[32];
+
+// Used by Web Server
+
+ESP8266WebServer server(80);
+static bool hasSD = false;
+File uploadFile;
+#define DBG_OUTPUT_PORT Serial
+const char* host = "boomy";
 
 void setup() {
   memset(broker_ip, 0, sizeof(broker_ip));
@@ -58,6 +69,7 @@ void setup() {
     Serial.println(F("SD failed, or not present"));
     while (1);  // don't do anything more
   }
+  hasSD = true;
   Serial.println("SD OK!");
   
   //writeWiFiCredentials("ssid", "password");
@@ -80,6 +92,23 @@ void setup() {
   mqtt.setServer(broker_ip, 1883);
   mqtt.setCallback(handle_mqtt_message);
 
+  if (MDNS.begin(host)) {
+    MDNS.addService("http", "tcp", 80);
+    Serial.println("MDNS responder started");
+    Serial.print("You can now connect to http://");
+    Serial.print(host);
+    Serial.println(".local");
+  }
+
+  server.on("/list", HTTP_GET, renderDirectory);
+  server.on("/edit", HTTP_DELETE, handleDelete);
+  server.on("/edit", HTTP_PUT, handleCreate);
+  server.on("/edit", HTTP_POST, [](){ returnOK(); }, handleFileUpload);
+  server.onNotFound(handleNotFound);
+
+  server.begin();
+  Serial.println("HTTP server started");
+  
   // If DREQ is on an interrupt pin we can do background
   // audio playing
   //musicPlayer.useInterrupt(VS1053_FILEPLAYER_PIN_INT);  // DREQ int
@@ -101,16 +130,8 @@ void loop() {
   }
   mqtt.loop();
 
-  WiFiClient client = server.available();
-  if(client) {
-    while(!client.available()) {
-      delay(1);
-    }
-    String req = client.readStringUntil('\r');
-    Serial.println(req);
-    client.flush();
-  }
-    
+  server.handleClient();
+
   // look for a message!
   if (irrecv.decode(&results)) {
     //Serial.println(results.value, HEX);
@@ -378,6 +399,10 @@ void handle_mqtt_message(char* topic, byte* payload, unsigned int length) {
     // TODO: Track dirty state?
     //mappingsDirty = true;
   }
+  size = sizeof("/remove/")-1;
+  if (strncmp((char*)payload, "/remove/", size) == 0) {
+    SD.remove((char*)&(payload[size]));
+  }
 }
 
 void reconnect() {
@@ -530,5 +555,182 @@ void list(File dir, int numTabs) {
      }
      entry.close();
    }
+}
+
+// Web Server
+
+void returnOK() {
+  server.send(200, "text/plain", "");
+}
+
+void returnFail(String msg) {
+  server.send(500, "text/plain", msg + "\r\n");
+}
+
+bool loadFromSdCard(String path){
+  String dataType = "text/plain";
+  if(path.endsWith("/")) path += "index.htm";
+
+  if(path.endsWith(".src")) path = path.substring(0, path.lastIndexOf("."));
+  else if(path.endsWith(".htm")) dataType = "text/html";
+  else if(path.endsWith(".css")) dataType = "text/css";
+  else if(path.endsWith(".js")) dataType = "application/javascript";
+  else if(path.endsWith(".png")) dataType = "image/png";
+  else if(path.endsWith(".gif")) dataType = "image/gif";
+  else if(path.endsWith(".jpg")) dataType = "image/jpeg";
+  else if(path.endsWith(".ico")) dataType = "image/x-icon";
+  else if(path.endsWith(".xml")) dataType = "text/xml";
+  else if(path.endsWith(".pdf")) dataType = "application/pdf";
+  else if(path.endsWith(".zip")) dataType = "application/zip";
+
+  File dataFile = SD.open(path.c_str());
+  if(dataFile.isDirectory()){
+    path += "/index.htm";
+    dataType = "text/html";
+    dataFile = SD.open(path.c_str());
+  }
+
+  if (!dataFile)
+    return false;
+
+  if (server.hasArg("download")) dataType = "application/octet-stream";
+
+  if (server.streamFile(dataFile, dataType) != dataFile.size()) {
+    DBG_OUTPUT_PORT.println("Sent less data than expected!");
+  }
+
+  dataFile.close();
+  return true;
+}
+
+void handleFileUpload(){
+  if(server.uri() != "/edit") return;
+  HTTPUpload& upload = server.upload();
+  if(upload.status == UPLOAD_FILE_START){
+    if(SD.exists((char *)upload.filename.c_str())) SD.remove((char *)upload.filename.c_str());
+    uploadFile = SD.open(upload.filename.c_str(), FILE_WRITE);
+    DBG_OUTPUT_PORT.print("Upload: START, filename: "); DBG_OUTPUT_PORT.println(upload.filename);
+  } else if(upload.status == UPLOAD_FILE_WRITE){
+    if(uploadFile) uploadFile.write(upload.buf, upload.currentSize);
+    DBG_OUTPUT_PORT.print("Upload: WRITE, Bytes: "); DBG_OUTPUT_PORT.println(upload.currentSize);
+  } else if(upload.status == UPLOAD_FILE_END){
+    if(uploadFile) uploadFile.close();
+    DBG_OUTPUT_PORT.print("Upload: END, Size: "); DBG_OUTPUT_PORT.println(upload.totalSize);
+  }
+}
+
+void deleteRecursive(String path){
+  File file = SD.open((char *)path.c_str());
+  if(!file.isDirectory()){
+    file.close();
+    SD.remove((char *)path.c_str());
+    return;
+  }
+
+  file.rewindDirectory();
+  while(true) {
+    File entry = file.openNextFile();
+    if (!entry) break;
+    String entryPath = path + "/" +entry.name();
+    if(entry.isDirectory()){
+      entry.close();
+      deleteRecursive(entryPath);
+    } else {
+      entry.close();
+      SD.remove((char *)entryPath.c_str());
+    }
+    yield();
+  }
+
+  SD.rmdir((char *)path.c_str());
+  file.close();
+}
+
+void handleDelete(){
+  if(server.args() == 0) return returnFail("BAD ARGS");
+  String path = server.arg(0);
+  if(path == "/" || !SD.exists((char *)path.c_str())) {
+    returnFail("BAD PATH");
+    return;
+  }
+  deleteRecursive(path);
+  returnOK();
+}
+
+void handleCreate(){
+  if(server.args() == 0) return returnFail("BAD ARGS");
+  String path = server.arg(0);
+  if(path == "/" || SD.exists((char *)path.c_str())) {
+    returnFail("BAD PATH");
+    return;
+  }
+
+  if(path.indexOf('.') > 0){
+    File file = SD.open((char *)path.c_str(), FILE_WRITE);
+    if(file){
+      file.write((const char *)0);
+      file.close();
+    }
+  } else {
+    SD.mkdir((char *)path.c_str());
+  }
+  returnOK();
+}
+
+void renderDirectory() {
+  if(!server.hasArg("dir")) return returnFail("BAD ARGS");
+  String path = server.arg("dir");
+  if(path != "/" && !SD.exists((char *)path.c_str())) return returnFail("BAD PATH");
+  File dir = SD.open((char *)path.c_str());
+  path = String();
+  if(!dir.isDirectory()){
+    dir.close();
+    return returnFail("NOT DIR");
+  }
+  dir.rewindDirectory();
+  server.setContentLength(CONTENT_LENGTH_UNKNOWN);
+  server.send(200, "text/json", "");
+  WiFiClient client = server.client();
+
+  server.sendContent("[");
+  for (int cnt = 0; true; ++cnt) {
+    File entry = dir.openNextFile();
+    if (!entry)
+    break;
+
+    String output;
+    if (cnt > 0)
+      output = ',';
+
+    char buffer[256];
+    output += "{\"type\":\"";
+    output += (entry.isDirectory()) ? "dir" : "file";
+    output += "\",\"name\":\"";
+    entry.getName(buffer, sizeof(buffer));
+    output += buffer;
+    output += "\"";
+    output += "}";
+    server.sendContent(output);
+    entry.close();
+ }
+ server.sendContent("]");
+ dir.close();
+}
+
+void handleNotFound(){
+  if(hasSD && loadFromSdCard(server.uri())) return;
+  String message = "SDCARD Not Detected\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET)?"GET":"POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i=0; i<server.args(); i++){
+    message += " NAME:"+server.argName(i) + "\n VALUE:" + server.arg(i) + "\n";
+  }
+  server.send(404, "text/plain", message);
+  DBG_OUTPUT_PORT.print(message);
 }
 
